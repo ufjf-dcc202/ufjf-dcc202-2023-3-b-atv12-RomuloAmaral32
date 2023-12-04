eecafd67459d18f4d91018a1f13a8013d435a584
RomuloAmaral32 <romuloamaral32@gmail.com>
2023-12-04 14:57:02 -0300

    configura o main.js adequadamente

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const olJoao = document.querySelector("ol#joao");
-const olMaria = document.querySelector("ol#maria");
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
 document.entrada.enviar.addEventListener('submit' , leformulario)
 
 function leformulario(event){
 event.preventDefault()
-const quantidade= document.entrada.quantidade.value;
-const quantidade= document.entrada.quantidade.valueAsnumber;
+const quantidade = document.entrada.quantidade.valueAsNumber;
 const fruta= document.entrada.fruta.value;
 const origem = document.entrada.origem.value;
 const destino = document.entrada.destino.value;
-console.log(`Eu tenho ${quantidade} ${fruta}`);
+console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 //document.entrada.submit();
 }
