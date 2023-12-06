7d0f0b0f0418524d1e469c0f71ee1409d07293a9
RomuloAmaral <romuloamaral32@gmail.com>
2023-12-05 21:45:21 -0300

    faz alteraçoes no main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+import { getEstoque } from "./estoque";
+
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
 document.entrada.enviar.addEventListener('submit' , leformulario)
+
+atualizatTela();
 
 function leformulario(event){
 event.preventDefault()
@@ -10,4 +14,14 @@ const origem = document.entrada.origem.value;
 const destino = document.entrada.destino.value;
 console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 //document.entrada.submit();
+}
+function atalizaTela(){
+    const estoque = getEstoque();
+olJoao.innerHTML="";
+for( let i=0; i<estoque.joao.length;i++){
+    const monte= estoque.joao[i];
+   const li = document.createElement('li');
+   li.textContent = `${monte.tipo}: ${monte.qtd}`;
+   olJoao.append(li);
+}
 }
