afab631f202b44804a0909c9b867712cdfb7932f
RomuloAmaral32 <romuloamaral32@gmail.com>
2023-12-10 19:41:31 -0300

    altera a main.js

--- main.js
+++ main.js
@@ -2,46 +2,52 @@ import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
-const btnLimpar = document.querySelector("#limpar");
 
 document.entrada.addEventListener('submit', leFormulario);
-btnLimpar.addEventListener('click', esvaziaLista);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
 
 atualizaTela();
 
-function leFormulario(event)
-{
-    event.preventDefault(event)
+function leFormulario(event) {
+    event.preventDefault(event);
     const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
-    
+
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 
     transacaoNoEstoque(origem, destino, fruta, quantidade);
     atualizaTela();
-    // document.entrada.submit();
 }
 
-function atualizaTela(){
-    const estoque = getEstoque();
-    preencheLista(olJoao, estoque.joao);
-    preencheLista(olMaria, estoque.maria);
-}
 
-function preencheLista(lista, estoqueDaPessoa){
-    lista.innerHTML = "";
-    for(let i = 0; i < estoqueDaPessoa.length; i++)
-    {
+function preencheLista(lista, estoqueDaPessoa) {
+    lista.textContent = "";
+
+    for (let i = 0; i < estoqueDaPessoa.length; i++) {
         const monte = estoqueDaPessoa[i];
         const li = document.createElement('li');
         li.textContent = `${monte.tipo}: ${monte.quantidade}`;
-        lista.append(li);
+        lista.appendChild(li);
     }
 }
 
-function esvaziaLista(event){
-    event.preventDefault(event)
-    console.log("Estoque Vazio");
+
+function atualizaTela() {
+    const estoque = getEstoque();
+
+    olJoao.innerHTML = "";
+    olMaria.innerHTML = "";
+    document.entrada.quantidade.value = 1;
+    document.entrada.fruta.value = "maca";
+
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
 }
